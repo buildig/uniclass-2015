384babc7acff1def5095055a24faacd16ad4d3e1
Tormi Tabor <tormi.tabor@gmail.com>
2017-08-04 22:48:19 +0300

    Remove version info from files (closes #6)

--- util/convert.sh
+++ util/convert.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
+# convert xlsx to csv
+# tail example: https://github.com/wireservice/csvkit/issues/336#issuecomment-56042714
+
 for i in *.xlsx;
  do
   filename=$(basename "$i" .xlsx);
-  outext=".csv" 
-  in2csv $i > $filename$outext
+  outext=".csv"
+  in2csv $i | tail -n +3 > $filename$outext
+done
+
+# remove versioning information from csv filenames
+# https://askubuntu.com/a/481848
+
+for i in *.csv
+do
+  newname="$(echo $i|cut -d_ -f1,2).csv";
+  cp $i $newname
 done
