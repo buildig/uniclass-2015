33f54401ea12e45611ded2792f729ef7890cf43e
Tormi Tabor <tormi.tabor@gmail.com>
2017-08-08 09:34:11 +0300

    July 2017 Update of convert.sh (#10)

--- util/convert.sh
+++ util/convert.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-# download xlsx files, April 2017 (change versions to latest before launching this script!)
+# Download July 2017 version of xlsx files 
+# (change version numbers to latest before launching this script!)
 
 nbsurl="https://toolkit.thenbs.com/Articles/Classification/Uniclass2015Table/"
 curl $nbsurl"Co?version=v1_3" > Uniclass2015_Co.xlsx &&
-curl $nbsurl"En?version=v1_4" > Uniclass2015_En.xlsx &&
-curl $nbsurl"Ac?version=v1_3" > Uniclass2015_Ac.xlsx &&
-curl $nbsurl"SL?version=v1_4" > Uniclass2015_SL.xlsx &&
+curl $nbsurl"En?version=v1_5" > Uniclass2015_En.xlsx &&
+curl $nbsurl"Ac?version=v1_4" > Uniclass2015_Ac.xlsx &&
+curl $nbsurl"SL?version=v1_5" > Uniclass2015_SL.xlsx &&
 curl $nbsurl"EF?version=v1_2" > Uniclass2015_EF.xlsx &&
-curl $nbsurl"Ss?version=v1_6" > Uniclass2015_Ss.xlsx &&
-curl $nbsurl"Pr?version=v1_6" > Uniclass2015_Pr.xlsx &&
-curl $nbsurl"TE?version=v1_1" > Uniclass2015_TE.xlsx &&
+curl $nbsurl"Ss?version=v1_7" > Uniclass2015_Ss.xlsx &&
+curl $nbsurl"Pr?version=v1_7" > Uniclass2015_Pr.xlsx &&
+curl $nbsurl"TE?version=v1_2" > Uniclass2015_TE.xlsx &&
 curl $nbsurl"Zz?version=v1_0" > Uniclass2015_Zz.xlsx &&
 curl $nbsurl"FI?version=v0_1" > Uniclass2015_FI.xlsx &&
 curl $nbsurl"PM?version=v0_1" > Uniclass2015_PM.xlsx
 
-# convert xlsx to csv
-# tail example: https://github.com/wireservice/csvkit/issues/336#issuecomment-56042714
+# Convert xlsx files to csv format
 
 for i in *.xlsx;
  do
