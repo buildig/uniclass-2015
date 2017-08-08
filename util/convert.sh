755ac54fcd8eb691dbe8238bc4fe06e6f8222c4a
Tormi Tabor <tormi.tabor@gmail.com>
2017-08-08 17:56:21 +0300

    Update convert.sh with Uniclass2015.csv creation script (#9)

--- util/convert.sh
+++ util/convert.sh
@@ -24,3 +24,19 @@ for i in *.xlsx;
   outext=".csv"
   in2csv $i | tail -n +3 > $filename$outext
 done
+
+# Download Uniclass2015_Tables.csv
+
+curl -O "https://raw.githubusercontent.com/buildig/uniclass-2015/master/Uniclass2015_Tables.csv"
+
+# Create Uniclass2015.csv
+
+uniclassfiles="Uniclass2015_Tables.csv Uniclass2015_Co.csv Uniclass2015_EF.csv
+Uniclass2015_En.csv Uniclass2015_FI.csv Uniclass2015_PM.csv Uniclass2015_Pr.csv
+Uniclass2015_SL.csv Uniclass2015_Ss.csv Uniclass2015_TE.csv Uniclass2015_Zz.csv"
+
+(head -1 Uniclass2015_Tables.csv;
+  for i in $uniclassfiles;
+    do tail -n +2 $i;
+  done
+) > Uniclass2015.csv
